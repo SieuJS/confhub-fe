155746d026c5173bff6de1a283895841ebd54205
21120321 <gamefun206@gmail.com>
2024-09-20 19:46:04 +0700

    fix api google

--- src/components/ButtonLoginWithGoogle/GoogleLoginButton.jsx
+++ src/components/ButtonLoginWithGoogle/GoogleLoginButton.jsx
@@ -5,6 +5,7 @@ import { Button, Image } from 'react-bootstrap';
 import googleIcon from './../../assets/imgs/google.png'
 import { useLocation } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
+import { baseURL } from '../../hooks/api/baseApi';
 
 const GoogleLoginButton = () => {
     const { t } = useTranslation()
@@ -25,7 +26,9 @@ const GoogleLoginButton = () => {
   }, [location.search]);
     
     const handleLoginWithGoogle = () => {
-       window.location.href = 'https://accounts.google.com/o/oauth2/v2/auth?response_type=code&redirect_uri=https%3A%2F%2Fconference-searching.onrender.com%2Fauth%2Fgoogle%2Fcallback&scope=profile%20email&client_id=332252746670-fh1iqdls3v0ka6plmrqaqam8q5l8se26.apps.googleusercontent.com';
+      //  window.location.href = 'https://accounts.google.com/o/oauth2/v2/auth?response_type=code&redirect_uri=http%3A%2F%2Fhttp://localhost:5000%2Fauth%2Fgoogle%2Fcallback&scope=profile%20email&client_id=540405177648-0tcg4ln5cq08l8nu797d52bt7n5i8ged.apps.googleusercontent.com';
+       window.location.href =  'http://localhost:5000/auth/google';
+       
     }
 
     return (
